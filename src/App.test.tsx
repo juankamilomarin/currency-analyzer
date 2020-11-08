c4342cd5cb80804096345bae94be543ae9b2295d
Juan Camilo Marin <juankamilomarin@gmail.com>
2020-11-07 23:43:51 -0500

    Fix App unit tests

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,21 +1,40 @@
-import React from 'react';
-import App from './App';
-import { render } from '@testing-library/react'; // TODO Add custom testUtils
+import React from 'react'
+import App from './App'
+import { render } from './testUtils'
 import AppBar from './app/AppBar'
+import { DateTime } from 'luxon'
+import ExchangeRateTable from './features/exchangeRate/components/ExchangeRateTable'
 
-const mockedAppBar = AppBar as jest.Mock
+const mockAppBar = AppBar as jest.Mock
 jest.mock("./app/AppBar", () => jest.fn())
+const mockExchangeRateTable = ExchangeRateTable as jest.Mock
+jest.mock("./features/exchangeRate/components/ExchangeRateTable", () => jest.fn())
 
 describe("App", () => {
 
     beforeEach(() => {
-      mockedAppBar.mockImplementation(() => <div data-testid="app-bar"/>)
+      mockAppBar.mockImplementation(() => <div data-testid="app-bar"/>)
+      mockExchangeRateTable.mockImplementation(() => <div data-testid="exchange-rate-table"/>)
     })
     
+    afterAll(() => {
+      jest.clearAllMocks()
+    })
 
     it("should render appBar", () => {
         const { getByTestId } = render(<App/>);
         expect(getByTestId("app-bar")).toBeInTheDocument();
     });
+
+    it("should render exchange rate table", () => {
+      const state = {
+        exchangeRate: {
+            loading: false,
+            selectedDate: DateTime.local()
+        }
+      }
+      const { getByTestId } = render(<App/>, state);
+      expect(getByTestId("exchange-rate-table")).toBeInTheDocument();
+  });
 
 });
